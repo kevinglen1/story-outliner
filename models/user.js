c4bcbf508aabaf0768732d025d1401455fb3a5f8
Kevin <kevinpeterglen@gmail.com>
2022-01-25 15:17:54 -0500

    added authentication and database

--- models/user.js
+++ models/user.js
@@ -2,18 +2,21 @@ var mongoose = require('mongoose');
 
 // The factSchema is used to embedded docs in as student doc.
 // There is no model and no 'facts' collection
+
+const cardSchema = new mongoose.Schema({
+  sceneType: String,
+  position: Number,
+  startingValue: String,
+  endingValue: String,
+  description: String,
+})
+
 const userSchema = new mongoose.Schema({
   googleId: String,
   name: String,
   cards: [cardSchema]
 })
 
-const cardSchema = new mongoose.Schema({
-  sceneType: String,
-  position: Int,
-  startingValue: String,
-  endingValue: String,
-  description: String,
-})
 
-module.exports = mongoose.model('Card', cardSchema);
+
+module.exports = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+const passport = require('passport')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// The root route renders our only view
+router.get('/', function(req, res) {
+  res.render('index', {user: null});
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email']}
+))
+
+router.get('/oauth2callback', passport.authenticate(
+  'google', 
+  {
+    successRedirect: '/',
+    failureRedirect: '/users'
+  }
+))
+
+router.get('/logout', function(req, res) {
+  req.logout()
+  res.redirect('/users')
+})
 
 module.exports = router;
